fe634c1d947207dd0b728840929854bc8aa99d6d
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-03-31 12:07:27 -0700

    correct the order of marking image as not writeable to improve performance

--- Python/live_stream_processing.py
+++ Python/live_stream_processing.py
@@ -12,8 +12,8 @@ def detect_landmarks(frame, holistic):
     Process the image with MediaPipe Holistic to detect landmarks.
     Returns the results which contain landmarks and image in RGB. 
     """
-    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     image_rgb.flags.writeable = False   
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Process the image and detect landmarks
     results = holistic.process(image_rgb)
