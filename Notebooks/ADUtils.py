d05ee4d1707d2168cea54a2099ad9b9f901b9139
Chanson Tang <90084138+chanchantang@users.noreply.github.com>
2024-04-15 19:14:39 -0700

    Cleaned up notebooks and updated model

--- Notebooks/ADUtils.py
+++ Notebooks/ADUtils.py
@@ -3,8 +3,12 @@ import numpy as np
 import os
 import mediapipe as mp
 
+"""
+Utility file for variables and functions used in data and model creation.
+"""
 
 # VARIABLES YOU MIGHT WANT TO CHANGE
+#--------------------------------
 
 # Path for exported data, numpy arrays
 DATA_PATH = os.path.join('../data/AD_data')
@@ -18,10 +22,7 @@ no_sequences = 30
 # Videos are going to be 30 frames in length
 sequence_length = 30
 
-
-
-
-
+#---------------------------------
 
 
 
@@ -34,20 +35,41 @@ mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    """
+    Processes an image to detect and extract landmarks using the MediaPipe Holistic model.
+
+    Parameters:
+        image: The image in which landmarks are to be detected.
+        holistic: The MediaPipe Holistic model object.
+
+    Returns:
+        results : An object containing detected landmarks such as pose, face, and hand landmarks.
+        image: The processed image.
+    """
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
 def draw_landmarks(image, results):
+    """
+    Draw the detected landmarks on the image.
+    This function is used in development mode.
+    Adapted from https://github.com/nicknochnack/Body-Language-Decoder/blob/main/Body%20Language%20Decoder%20Tutorial.ipynb
+    """
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS) # Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
 
 def draw_styled_landmarks(image, results):
+    """
+    Draw the detected landmarks on the image STYLISHLY.
+    This function is used in development mode.
+    Adapted from https://github.com/nicknochnack/Body-Language-Decoder/blob/main/Body%20Language%20Decoder%20Tutorial.ipynb
+    """
     # Draw face connections
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
@@ -70,6 +92,23 @@ def draw_styled_landmarks(image, results):
                              )
 
 def extract_keypoints(results):
+    """
+    Extracts keypoints from the pose, left hand, and right hand landmarks from the provided results.
+
+    This function processes pose and hand landmarks detected in an image or video frame, converting them into
+    a flat numpy array. If landmarks for a specific part (pose, left hand, right hand) are not found,
+    it fills the respective part of the array with zeros.
+
+    Parameters:
+        results: A result object from MediaPipe Holistic containing various landmark detections.
+
+    Returns:
+        numpy.ndarray: A 1D array containing all the extracted keypoints for pose, left hand, and right hand.
+                       Each keypoint consists of its x, y, z coordinates, and a visibility score (for pose only).
+                       If no keypoints are detected for a specific part, that segment of the array is filled with zeros.
+                       The pose segment is 132 elements long (33 keypoints * 4 attributes each), and each hand
+                       segment is 63 elements long (21 keypoints * 3 attributes each).
+    """
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
 #     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
